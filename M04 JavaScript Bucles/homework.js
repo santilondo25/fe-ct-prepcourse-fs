883765243c53008193474b2efb9c79c017f6484c
santilondo25 <santilondov25@gmail.com>
2023-02-13 16:41:32 -0500

    M04

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -167,6 +167,22 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if ( num1 < 0 || num2 < 0 || num3 < 0 ){
+      return "Hay negativos";
+   }
+   else if ( num1 > num2 && num1 > num3 ){
+      return "Numero 1 es mayor y positivo";
+   }
+   else if ( num3 > num2 && num3 > num1 ){
+      num3 = num3 + 1;
+      return num3;
+   }
+   else if ( num1 === 0 && num2 === 0 && num3 === 0){
+      return "Error";
+   }
+   else {
+      return false;
+   }
 }
 
 function esPrimo(num) {
@@ -176,18 +192,43 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+  
+   if ( num <= 1 ){
+      return false;
+   }
+   for ( var i = 2; i < num; i ++ ){
+   
+      if ( num % i === 0) {
+         return false;
+      }
+      }
+      return true;
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   
+   if ( valor === true){
+      return "Soy verdadero";
+   }
+   else {
+      return "Soy falso";
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   
+   if ( num >=100 && num <= 999 ){
+      return true;
+   }
+   else {
+      return false;
+   }
 }
 
 function doWhile(num) {
@@ -195,6 +236,13 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+    var i = 0;
+    do {
+      i++;
+      num = num + 5;
+    } 
+    while ( i < 8);
+    return num;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
